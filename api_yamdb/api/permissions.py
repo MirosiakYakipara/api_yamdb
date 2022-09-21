7a8a46874d9abf1ef461dfae8a7ca5153e623eb4
aleksandr.buliga <buliga.alexander@yandex.ru>
2022-09-22 01:47:11 +0400

    model-permissions

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -6,3 +6,35 @@ class IsAdminOrReadOnly(permissions.BasePermission):
         return (request.method in permissions.SAFE_METHODS
                 or (request.user.is_authenticated and (
                     request.user.is_admin or request.user.is_superuser)))
+
+
+class UserPermission(permissions.BasePermission):
+
+    def has_permission(self, request, view):
+        if view.action == 'list':
+            return True
+        # elif view.action == 'create':
+        #     return request.user.is_authenticated()
+        # elif view.action in ['retrieve', 'update', 'partial_update', 'destroy']:
+        #    return request.user.is_authenticated()
+        elif view.action == 'create':
+            return True
+        elif view.action == 'retrive':
+            return True
+        else:
+            return False
+                                                                                                
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        
+        if view.action == 'retrieve':
+            return obj.author == request.user or request.user.is_admin
+        elif view.action == 'create':
+            return True
+        elif view.action in ['update', 'partial_update']:
+            return obj.author == request.user or request.user.is_admin
+        elif view.action == 'destroy':
+            return obj.author == request.user or request.user.is_admin
+        else:
+            return False
--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,12 +1,22 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .views import CategoryViewSet, GenreViewSet, TitleViewSet
+from .views import CategoryViewSet, GenreViewSet, TitleViewSet, ReviewViewSet, CommentViewSet
 
 router = DefaultRouter()
 router.register(r'categories', CategoryViewSet)
 router.register(r'genres', GenreViewSet)
 router.register(r'titles', TitleViewSet)
+router.register("titles/(?P<title_id>\\d+)/reviews", ReviewViewSet, basename="reviews")
+router.register("titles/(?P<title_id>\\d+)/reviews/(?P<review_id>\\d+)/comments",
+                CommentViewSet,
+                basename="comments"
+                )
+router.register("titles/(?P<title_id>\\d+)/reviews/(?P<review_id>\\d+)/comments/(?P<comment_id>\\d+)",
+                CommentViewSet,
+                basename="comments"
+                )
+
 
 urlpatterns = [
     path('v1/', include(router.urls)),
