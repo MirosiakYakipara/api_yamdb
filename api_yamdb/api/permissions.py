ffb0738027d019938272c4b584c782e6becc64f9
aleksandr.buliga <buliga.alexander@yandex.ru>
2022-09-22 18:54:49 +0400

    model-permissions

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -13,12 +13,10 @@ class UserPermission(permissions.BasePermission):
     def has_permission(self, request, view):
         if view.action == 'list':
             return True
-        # elif view.action == 'create':
-        #     return request.user.is_authenticated()
-        # elif view.action in ['retrieve', 'update', 'partial_update', 'destroy']:
-        #    return request.user.is_authenticated()
         elif view.action == 'create':
-            return True
+            return request.user.is_authenticated
+        elif view.action in ['retrieve', 'update', 'partial_update', 'destroy']:
+           return request.user.is_authenticated
         elif view.action == 'retrive':
             return True
         else:
@@ -27,14 +25,26 @@ class UserPermission(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
+
+        request_user_is_admin: bool = False
+        try:
+            request_user_is_admin = request.user.role == 'admin'
+        except:
+            pass
+        
+        request_user_is_moderator: bool = False
+        try:
+            request_user_is_moderator = request.user.role =='moderator'
+        except:
+            pass
         
         if view.action == 'retrieve':
-            return obj.author == request.user or request.user.is_admin
+            return obj.author == request.user or request_user_is_admin or request_user_is_moderator
         elif view.action == 'create':
             return True
         elif view.action in ['update', 'partial_update']:
-            return obj.author == request.user or request.user.is_admin
+            return obj.author == request.user or request_user_is_admin or request_user_is_moderator
         elif view.action == 'destroy':
-            return obj.author == request.user or request.user.is_admin
+            return obj.author == request.user or request_user_is_admin or request_user_is_moderator
         else:
             return False
