cd18f824f8747a73bd84d0dd4aaf47e55feb81de
aleksandr.buliga <buliga.alexander@yandex.ru>
2022-09-22 22:50:59 +0400

    fix/urls

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -8,10 +8,6 @@ router.register(r'categories', CategoryViewSet)
 router.register(r'genres', GenreViewSet)
 router.register(r'titles', TitleViewSet)
 router.register("titles/(?P<title_id>\\d+)/reviews", ReviewViewSet, basename="reviews")
-router.register("titles/(?P<title_id>\\d+)/reviews/(?P<review_id>\\d+)/comments",
-                CommentViewSet,
-                basename="comments"
-                )
 router.register("titles/(?P<title_id>\\d+)/reviews/(?P<review_id>\\d+)/comments/(?P<comment_id>\\d+)",
                 CommentViewSet,
                 basename="comments"
